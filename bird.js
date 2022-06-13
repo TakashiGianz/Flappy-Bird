3c3073a4426af7d7bd626f1ee9a854bdb528288d
TakashiGianz <fernando.takashi12@gmail.com>
2022-06-13 17:21:28 -0300

    Bird basic settings

---
+++ bird.js
@@ -0,0 +1,27 @@
+const birdElem = document.querySelector("[data-bird]");
+const BIRD_SPEED = 0.3;
+
+export function setupBird() {
+  setTop(window.innerHeight / 2);
+  document.removeEventListener("keydown", handleJump);
+  document.addEventListener("keydown", handleJump);
+}
+
+export function updateBird(delta) {
+  setTop(getTop() + BIRD_SPEED * delta);
+  console.log(getTop());
+}
+
+function setTop(top) {
+  birdElem.style.setProperty("--bird-top", top);
+}
+
+function getTop() {
+  return parseFloat(getComputedStyle(birdElem).getPropertyValue("--bird-top"));
+}
+
+function handleJump(e) {
+  if (e.code !== "Space") {
+    return;
+  }
+}
